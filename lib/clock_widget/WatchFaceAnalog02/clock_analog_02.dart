cc09371da2e1c195f4f906b2a2777b79fa9b9f13
raymond <raymondtruongdev@gmail.com>
2024-01-30 13:56:03 +0700

    second hand update each 20ms (50 fps), add higher resolution images

--- lib/clock_widget/WatchFaceAnalog02/clock_analog_02.dart
+++ lib/clock_widget/WatchFaceAnalog02/clock_analog_02.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:watch_launcher/controller.dart';
 
 class ClockAnalog02 extends StatefulWidget {
   const ClockAnalog02({super.key});
@@ -18,8 +20,8 @@ class _ClockAnalog02State extends State<ClockAnalog02> {
   void initState() {
     super.initState();
     _dateTime = DateTime.now();
-    _timer =
-        Timer.periodic(const Duration(seconds: 1), (Timer t) => _updateTime());
+    _timer = Timer.periodic(
+        const Duration(milliseconds: 20), (Timer t) => _updateTime());
   }
 
   void _updateTime() {
@@ -36,42 +38,76 @@ class _ClockAnalog02State extends State<ClockAnalog02> {
 
   @override
   Widget build(BuildContext context) {
-    const pivotOffsetMinute = Offset(0, 70);
-    const pivotOffsetSecond = Offset(0, 51.5);
-    const pivotOffsetHour = Offset(0, 49.5);
-    var secondAngle = _dateTime.second * 6.0 * pi / 180;
-    var minuteAngle =
-        (_dateTime.minute * 6 + _dateTime.second * 0.1) * 0.0174533;
-    final hourAngle =
-        (_dateTime.hour * 30 + _dateTime.minute * 0.5) * 0.0174533; // Hour hand
+    final GlobalController globalController =
+        Get.put(GlobalController(), permanent: true);
+
+    double watchSize = (globalController.getWatchSize().value);
+
+    const pivotOffsetMinute = Offset(0, 0);
+    const pivotOffsetSecond = Offset(0, 0);
+    const pivotOffsetHour = Offset(0, 0);
+
+    var totalSeconds = _dateTime.second + (_dateTime.millisecond / 1000.0);
+    var secondAngle = totalSeconds * 6.0 * pi / 180;
+    var milisecondAngle = _dateTime.millisecond / 1000.0 * 6.0 * pi / 180;
+    var minuteAngle = (_dateTime.minute * 6 + totalSeconds * 0.1) * 0.0174533;
+    final hourAngle = (_dateTime.hour * 30 + _dateTime.minute * 0.5) *
+        0.0174533; // Hour handr hand
 
     return Stack(
       alignment: Alignment.center,
       children: <Widget>[
         Transform(
-          alignment: Alignment.center,
-          transform: Matrix4.identity()
-            ..rotateZ(hourAngle)
-            ..translate(-pivotOffsetHour.dx, -pivotOffsetHour.dy),
-          child: Image.asset(
-              'lib/clock_widget/WatchFaceAnalog02/image/hour_hand.png'), // Clock hand image
-        ),
+            alignment: Alignment.center,
+            transform: Matrix4.identity()
+              ..rotateZ(hourAngle)
+              ..translate(-pivotOffsetHour.dx, -pivotOffsetHour.dy),
+            child: Container(
+              width: watchSize,
+              height: watchSize,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(
+                      'lib/clock_widget/WatchFaceAnalog02/image/hour_hand.png'),
+                  fit: BoxFit.contain,
+                ),
+              ),
+            ) // Clock hand image
+            ),
         Transform(
-          alignment: Alignment.center,
-          transform: Matrix4.identity()
-            ..rotateZ(minuteAngle)
-            ..translate(-pivotOffsetMinute.dx, -pivotOffsetMinute.dy),
-          child: Image.asset(
-              'lib/clock_widget/WatchFaceAnalog02/image/minute_hand.png'), // Clock hand image
-        ),
+            alignment: Alignment.center,
+            transform: Matrix4.identity()
+              ..rotateZ(minuteAngle)
+              ..translate(-pivotOffsetMinute.dx, -pivotOffsetMinute.dy),
+            child: Container(
+              width: watchSize,
+              height: watchSize,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(
+                      'lib/clock_widget/WatchFaceAnalog02/image/minute_hand.png'),
+                  fit: BoxFit.contain,
+                ),
+              ),
+            ) // Clock hand image
+            ),
         Transform(
-          alignment: Alignment.center,
-          transform: Matrix4.identity()
-            ..rotateZ(secondAngle)
-            ..translate(-pivotOffsetSecond.dx, -pivotOffsetSecond.dy),
-          child: Image.asset(
-              'lib/clock_widget/WatchFaceAnalog02/image/second_hand.png'), // Clock hand image
-        ),
+            alignment: Alignment.center,
+            transform: Matrix4.identity()
+              ..rotateZ(secondAngle)
+              ..translate(-pivotOffsetSecond.dx, -pivotOffsetSecond.dy),
+            child: Container(
+              width: watchSize,
+              height: watchSize,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(
+                      'lib/clock_widget/WatchFaceAnalog02/image/second_hand.png'),
+                  fit: BoxFit.contain,
+                ),
+              ),
+            ) // Clock hand image
+            ),
       ],
     );
   }
